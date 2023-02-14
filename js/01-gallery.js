5f71e7108738061b8ac746a1ec475fe00c757b3e
mafioz359 <64733979+mafioz359@users.noreply.github.com>
2023-02-14 16:17:03 +0300

    дз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,5 +2,36 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryEl = document.querySelector(".gallery");
+const createNewImg = ({ preview, original, description }) => `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+const galleryEls = galleryItems.map(createNewImg).join("");
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryEls);
+galleryEl.addEventListener("click", onPaletteConteinerClick);
+function onPaletteConteinerClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+  galleryEl.addEventListener("keydown", escCloseFoto);
+  function escCloseFoto(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+      galleryEl.removeEventListener("keydown", escCloseFoto);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galeryImg = ({ preview, original, description }) =>
+  `<a class="gallery__item" href="${original}"> 
+    <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+const galleryEls = galleryItems.map(galeryImg).join("");
+galleryEl.insertAdjacentHTML("afterbegin", galleryEls);
+let gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function () {
+  // do something…
+});
